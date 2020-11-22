8a3756cc4acb5e753ac55eab0db81e24e2dc71ab
Gauthier Billot <gogoprog@gmail.com>
2020-11-22 21:47:05 +0100

    Wip

--- src/cac/Lib.hx
+++ src/cac/Lib.hx
@@ -1,9 +1,32 @@
 package cac;
 
+import js.Browser.document;
+
 @:expose("cac")
 class Lib {
-    static public function initialize() {
-        trace("cocou");
-    }
+    static public function initialize(root:js.html.Element) {
+        var currentDay = 5;
 
+        for(i in 0...24) {
+            var e = document.createElement("div");
+            e.className = "day";
+            {
+                var gift = document.createElement("div");
+
+                if(i < currentDay) {
+                    gift.className = "opened-gift";
+                } else {
+                    gift.className = "gift";
+                }
+
+                e.appendChild(gift);
+            }
+            {
+                var span = document.createElement("span");
+                span.innerText = (i + 1) +"";
+                e.appendChild(span);
+            }
+            root.appendChild(e);
+        }
+    }
 }
