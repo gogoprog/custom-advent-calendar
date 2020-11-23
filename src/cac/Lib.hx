21982c93eeafbe0933800174c5ce95ec30f591b4
Gauthier Billot <gogoprog@gmail.com>
2020-11-23 21:15:39 +0100

    Functional

--- src/cac/Lib.hx
+++ src/cac/Lib.hx
@@ -5,8 +5,19 @@ import js.Browser.window;
 
 @:expose("cac")
 class Lib {
+    static private var data:Dynamic;
+
+    static function main() {
+        initialize(document.getElementById("root"));
+    }
+
     static public function initialize(root:js.html.Element) {
-        var currentDay = 5;
+        var currentDay = -666;
+        var today = Date.now();
+
+        if(today.getMonth() == 11) {
+            currentDay = today.getDate() - 1;
+        }
 
         for(i in 0...24) {
             var e = document.createElement("div");
@@ -26,18 +37,24 @@ class Lib {
                     gift.appendChild(span);
                 }
 
-                gift.addEventListener("click", function(e) {
-                    trace(i);
-                });
+                if(i <= currentDay) {
+                    gift.addEventListener("click", function(e) {
+                        openDay(i);
+                        gift.className = "gift opened-gift";
+                        e.stopPropagation();
+                    });
+                }
+
                 e.appendChild(gift);
             }
             root.appendChild(e);
         }
 
         loadData();
+        document.addEventListener("click", closeMessage);
     }
 
-    static public function getParameter(name:String):String {
+    static private function getParameter(name:String):String {
         var urlString = window.location.href;
         var url = new js.html.URL(urlString);
         return url.searchParams.get(name);
@@ -45,18 +62,34 @@ class Lib {
 
     static private function loadData() {
         var data = getParameter("data");
-
         var http = new haxe.Http(data);
         http.onData = function(text) {
             var data = haxe.Json.parse(text);
-
             applyData(data);
         };
-
         http.request();
     }
 
     static private function applyData(data) {
         document.getElementById("title").innerText = data.title;
+        Lib.data = data;
+    }
+
+    static private function openDay(index:Int) {
+        var message = document.getElementById("message");
+        var span = document.querySelector(".message span");
+        span.innerText = "" + (index+1);
+        var p = document.querySelector(".message p");
+        p.innerText = data.days[index];
+        message.style.display = "flex";
+        haxe.Timer.delay(function() {
+            message.className = "message appear";
+        }, 0);
+    }
+
+    static private function closeMessage(e) {
+        var message = document.getElementById("message");
+        message.style.display = "none";
+        message.className = "message";
     }
 }
